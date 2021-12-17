0c4a6f0fa6b2b46967a172e32a187231be053d14
‎‎‎‎‎‎    ‎‎‎‎‎‎    ‎‎‎‎‎‎ ‎‎‎‎‎‎    ‎‎‎‎‎‎    ‎‎‎‎‎‎   ‎‎‎‎‎‎    ‎‎‎‎‎‎    ‎‎‎‎‎‎     ‎‎‎‎‎‎    ‎‎‎‎‎‎       E  L  M  I  R  A <71405939+elmiracodes@users.noreply.github.com>
2021-12-17 02:15:05 +0100

    Create header.php

---
+++ header.php
@@ -0,0 +1,41 @@
+<!--En fil för menysystemet/headern -->
+<?php
+include 'minstil.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Dental West</title>
+    <link rel="stylesheet" href="~/lib/bootstrap/dist/css/bootstrap.min.css" />
+    <link rel="minstil" type="text/php" href="minstil.php">
+</head>
+
+<!-- Nedan är menyn i headern -->
+<body>
+<header>
+<nav class="headern">
+    <div class="headercon">
+        <a class="snow" href="hem.php">DENTAL WEST <span class="sr-only"></span></a>  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; 
+        &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; 
+        &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; 
+        &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; 
+        &nbsp; &nbsp; &nbsp;  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;  &nbsp; &nbsp; 
+        &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; 
+        &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; 
+        <a class="snow" href="behandlingar.php">BEHANDLINGAR</a> &nbsp; <a class="snow" href="prislista.php">PRISLISTA</a> &nbsp; <a class="snow" href="minasidor.php">MINA SIDOR</a> &nbsp; <a class="snow" href="kontakt.php">KONTAKT</a>
+
+    </div>
+</nav>
+</header>
+</div>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+
+</body>
+</html>
+
