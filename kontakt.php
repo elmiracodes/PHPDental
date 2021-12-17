2093fa14ed5a7b053da9aca3ce2403142500f3df
‎‎‎‎‎‎    ‎‎‎‎‎‎    ‎‎‎‎‎‎ ‎‎‎‎‎‎    ‎‎‎‎‎‎    ‎‎‎‎‎‎   ‎‎‎‎‎‎    ‎‎‎‎‎‎    ‎‎‎‎‎‎     ‎‎‎‎‎‎    ‎‎‎‎‎‎       E  L  M  I  R  A <71405939+elmiracodes@users.noreply.github.com>
2021-12-17 02:14:28 +0100

    Create kontakt.php

---
+++ kontakt.php
@@ -0,0 +1,20 @@
+<?php
+include 'minstil.php';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width" />
+    <title>Index</title>
+</head>
+<body>
+<div>
+<img style="-webkit-user-select: none; margin: auto;" src="https://cdn1.cdnme.se/4825675/9-3/kontakt_5f79fc9c9606ee7873bdd3d5.jpg" width="950" height="470" />
+</div>
+
+
+</body>
+</html>
+
+
+
