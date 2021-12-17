bb15b325a5d6e9c6a6fefb9b7a2e765dd57643ca
‎‎‎‎‎‎    ‎‎‎‎‎‎    ‎‎‎‎‎‎ ‎‎‎‎‎‎    ‎‎‎‎‎‎    ‎‎‎‎‎‎   ‎‎‎‎‎‎    ‎‎‎‎‎‎    ‎‎‎‎‎‎     ‎‎‎‎‎‎    ‎‎‎‎‎‎       E  L  M  I  R  A <71405939+elmiracodes@users.noreply.github.com>
2021-12-17 02:13:16 +0100

    Create minstil.php

---
+++ minstil.php
@@ -0,0 +1,176 @@
+<!-- Denna fil står endast för CSS designen -->
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <link rel="minstil" type="text/php" href="minstil.php">
+    <title>Dental </title>
+
+<style type="text/css">
+
+h1 { 
+    color: deepskyblue;
+    font-weight: bold;
+    font-family: Bookman Old Style;
+}
+
+h2 {
+    text-align: left;
+    margin-bottom: 40px;
+    font-family: Bookman Old Style;
+
+}
+
+h3 {
+   
+    color: deepskyblue;
+    font-weight: bold;
+    font-style: italic;
+    font-family: Cambria Style;
+}
+
+
+#Corona{
+    font-family: Bookman Old Style;
+    color: dimgray;
+}
+
+.CoronaTab {
+ 
+ background-color: white;
+
+}
+.center{
+    border-width: thick;
+    margin-right: auto;
+    margin-left: auto;
+}
+
+th{
+    color: #ff9900;
+}
+
+td{
+    color: #666600;
+}
+
+div{
+    color: #666600;
+}
+
+#link{
+    text-align: center;
+}
+
+
+.co {
+    background-color: deepskyblue;
+}
+
+.headern {
+   color: white;
+   background-color: #11BAC3;
+   font-family: Cambria Style;; 
+   padding: 25px;
+}
+
+
+.foot {
+    color: white;
+    background-color: #11BAC3;
+    font-family: Cambria Style;
+}
+
+.snow {
+    color: white;
+}
+
+#frm {
+    border: solid gray 1px;
+    width: 20%;
+    border-radius: 5px;
+    margin: 100px auto;
+    background: white;
+    padding: 50px;
+}
+
+#btn {
+color: #fff;
+background: #337ab7;
+padding: 5px;
+margin-left: 69%;
+
+}
+
+
+#loginbody {
+    background: #1690A7;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+}
+
+*{
+    font-family: sans-serif;
+    box-sizing: border-box;
+}
+
+
+form {
+    width: 500px;
+    border: 2px solid #ccc;
+    padding: 30px;
+    background: #fff;
+    border-radius: 15px;
+}
+
+input {
+    display: block;
+    border: 2px solid #ccc;
+    width: 95%;
+    padding: 10px;
+    margin: 10px auto;
+    border-radius: 5px;
+}
+label {
+    color: #888;
+    font-size: 18px;
+    padding: 10px;
+}
+
+button {
+    float: right;
+background: #555;
+padding: 10px 15px;
+color: #fff;
+border-radius: 5px;
+margin-right: 10px;
+border: none;
+}
+
+button:hover{
+    opacity: .7;
+}
+
+
+</style>
+</head>
+<body>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
