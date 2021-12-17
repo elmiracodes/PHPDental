3e6babe8ad14af248f5abdb841e914e13230f367
‎‎‎‎‎‎    ‎‎‎‎‎‎    ‎‎‎‎‎‎ ‎‎‎‎‎‎    ‎‎‎‎‎‎    ‎‎‎‎‎‎   ‎‎‎‎‎‎    ‎‎‎‎‎‎    ‎‎‎‎‎‎     ‎‎‎‎‎‎    ‎‎‎‎‎‎       E  L  M  I  R  A <71405939+elmiracodes@users.noreply.github.com>
2021-12-17 02:14:43 +0100

    Create hem.php

---
+++ hem.php
@@ -0,0 +1,47 @@
+<!-- Hemsidan innehåller footer längst ner. Headern är inkluderad-->
+<?php
+include 'minstil.php';
+include 'header.php';
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <link rel="minstil" type="text/php" href="minstil.php">
+    <title>Dental </title>
+</head>
+
+<body id="class">
+<img style="-webkit-user-select: none; margin: auto;" src="https://cdn2.cdnme.se/4825675/9-3/smile_-_kopia_5f79fca29606ee784a09720b.png" width="100%" height="40%" />
+<h1>Välkommen till Dental West</h1><h3>Din tandklinik i hjärtat av Trollhättan City</h3>
+<table class="CoronaTab" style="width:55%">
+        <tr>
+            <td id="Corona">
+                Hej patient! <br /> Vi hoppas du mår bra under de rådande omständigheterna! Vi följer, precis som du, utvecklingen i världen. Vi vill med detta meddelande bara göra dig uppmärksam på att vi håller öppet som vanligt. Vi tar självfallet de rekommenderade försiktighetsåtgärderna och hygienrutinerna på största allvar.
+                Om du inte känner dig frisk eller tillhör riskgrupp så hjälper vi dig naturligtvis att omboka din tid. Om ni har frågor eller funderingar är ni välkomna att höra av er till oss! Vi vill ge dig, oavsett ålder, denna chans till god tandhälsa.
+                Vi ser fram emot att träffa dig, välkommen! /Dental West 
+            </td>
+        </tr>
+    </table>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+
+
+
+<footer class="foot">
+        <div class="snow"><br/>
+            &copy; 2020 - Dental West - <a class="nav-link snow" href="kontakt.php">Kod & Form av E </a>
+            <p class="snow">Kontakt: hej@dentalwest.se</p>
+
+        </div>
+</footer>
+            
+</body>
+</html>
+
+
+    
